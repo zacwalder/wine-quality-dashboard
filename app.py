50b081aa0340dacd52a984ab5965eb3f8121e7c6
Zacharias Walder Michalas <150163428+zacwalder@users.noreply.github.com>
2026-07-02 16:54:13 +0300

    Fix: remove unused seaborn import

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 import joblib
 import matplotlib.pyplot as plt
-import seaborn as sns
 import shap
 import plotly.express as px
 import plotly.graph_objects as go
